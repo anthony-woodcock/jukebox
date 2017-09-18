6e2aa76355ecbc64e9845359ccdcae28ae17f71e
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-09-18 20:39:42 +0100

    login page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import Login from './components/Login'
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      gAuthInstance: null,
+      authenticatedUser: null
+    }
+  }
+
+  componentDidMount () {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        'apiKey': process.env.REACT_APP_GOOGLE_API_KEY,
+        'clientId': process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        'scope': 'https://www.googleapis.com/auth/youtube',
+        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+      }).then(() => {
+        this.setState({
+          gAuthInstance: window.gapi.auth2.getAuthInstance()
+        })
+
+        this.state.gAuthInstance.isSignedIn.listen(this.handleAuthorization)
+        this.handleAuthorization()
+      }).catch(error => console.log(error))
+    })
+  }
+
+  handleAuthorization () {
+    this.setState({
+      authenticatedUser: this.state.gAuthInstance.currentUser.get()
+    })
+  }
+
   render() {
+    if (this.state.authenticatedUser) {
+      return (
+        <div className='App'>
+          You are logged in!
+        </div>
+      )
+    }
+
     return (
-      <div className="app"></div>
-       
+      <div className='App'>
+        <Login gAuthInstance={this.state.gAuthInstance} />
+      </div> 
     )
   }
 }
---
+++ src/components/Login.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import './Login.css'
+
+class Login extends Component {
+
+    constructor () {
+        super()
+
+        this.handleSignIn = this.handleSignIn.bind(this)
+    }
+
+    handleSignIn () {
+        this.props.gAuthInstance.signIn()
+    }
+
+    render() {
+        console.log(this)
+            if (this.props.gAuthInstance) {
+                return (
+                    <div className="Login">
+                        <div className="inner">
+                            <h1>Jukebox</h1>
+                            <button className="youtube-button" onClick={this.handleSignIn}><i className="fa fa-youtube-play" aria-hidden="true"></i> Sign in to YouTube</button>
+                        </div>
+                    </div>
+                )
+            }
+
+            return (
+                <div className="Login">
+                    <div className="inner">
+                        Loading.......
+                    </div>
+                </div>
+            )
+    }
+
+}
+
+export default Login
